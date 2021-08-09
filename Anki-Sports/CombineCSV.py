fea980963dbf73b410c0c778912fcb6c03782420
Mike Nolan <mike.nolan.joshua@gmail.com>
2021-08-08 17:59:07 -0700

    updated cards for clarity

--- Anki-Sports/CombineCSV.py
+++ Anki-Sports/CombineCSV.py
@@ -3,21 +3,22 @@ import os
 
 csv_out = 'consolidated.csv'
 csv_dir = os.getcwd()
-
-dir_tree = os.walk(csv_dir)
-for dirpath, dirnames, filenames in dir_tree:
-   pass
-
-csv_list = []
-for file in filenames:
-   if file.endswith('.csv'):
-      csv_list.append(file)
+print(csv_dir)
 
 csv_merge = open(csv_out, 'w')
+dir_tree = os.walk(csv_dir)
+for dirpath, dirnames, filenames in dir_tree:
+    csv_list = []
+    for file in filenames:
+       if file.endswith('.csv') and not file == 'consolidated.csv':
+          print(file)
+          csv_list.append(file)
 
-for file in csv_list:
-   csv_in = open(file)
-   for line in csv_in:
-      csv_merge.write(line)
-   csv_in.close()
+    
+
+    for file in csv_list:
+       csv_in = open(file)
+       for line in csv_in:
+          csv_merge.write(line)
+       csv_in.close()
 csv_merge.close()
