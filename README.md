ad4beef1edeb2c61d8ba956659b554be93db31ba
Michael Nolan <mike.nolan.joshua@gmail.come>
2024-01-26 18:21:09 -0600

    [SideEffect] move repos

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Public Projects
 
+* [SideEffect](SideEffect/README.md) - Making side effects and function purity a part of the Java type system.
 * [Simple Chaos](SimpleChaos/README.md)
 * [AnkiHelpBot](AnkiHelpBot/README.md)
 * [Anki Sports](Anki-Sports/README.md)
---
+++ SideEffect/src/main/java/dev/noblehouse/side_effect/annotations/SideEffectIgnore.java
@@ -0,0 +1,10 @@
+package dev.noblehouse.side_effect.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.METHOD})
+public @interface SideEffectIgnore {}
